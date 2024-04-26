a95974af8074b02594dbb959e6c7322aa226b1a6
EvgeniiBazalev <bazalevj@gmail.com>
2024-04-26 08:32:43 +0500

    3

--- 2024-04-25.js
+++ 2024-04-25.js
@@ -1,17 +1,13 @@
-function narcissistic(value) {
-  let arrDigits = [...String(value)].map((item) => +item);
-  let raisedArr = arrDigits.map((item) => item ** arrDigits.length);
-  console.log(raisedArr);
+function isValidWalk(walk) {
+  //insert brilliant code here
 
-  let sumRaisedArr = raisedArr.reduce(function (accumulator, item) {
-    return accumulator + item;
-  }, 0);
+  let N = walk.filter((item) => item == "n").length;
+  let S = walk.filter((item) => item == "s").length;
+  let W = walk.filter((item) => item == "w").length;
+  let E = walk.filter((item) => item == "e").length;
 
-  if (sumRaisedArr == value) {
-    return true;
-  } else {
-    return false;
-  }
+  if ((N == S) & (W == E) & (N + S + W + E == 10)) return true;
+
+  return false;
 }
-
-console.log(narcissistic(137));
+console.log(isValidWalk(["n", "s", "n", "s", "n", "s", "n", "s", "n", "s"]));
