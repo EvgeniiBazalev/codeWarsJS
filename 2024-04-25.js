ef66bda7626d4db2d56c2fff61cd07d376f04905
EvgeniiBazalev <bazalevj@gmail.com>
2024-05-02 11:11:52 +0500

    1

--- 2024-04-25.js
+++ 2024-04-25.js
@@ -1,11 +1,19 @@
-function tribonacci(signature, n) {
-  if (n > 3) {
-    for (i = 0; i < n - 3; i++) {
-      signature.push(signature[i + 0] + signature[i + 1] + signature[i + 2]);
+function pickPeaks(arr) {
+  let result = { pos: [], peaks: [] };
+  let candidate = null;
+  for (i = 1; i <= arr.length; i++) {
+    if (arr[i] - arr[i - 1] > 0) {
+      candidate = { pos: i, peaks: arr[i] };
     }
-  } else {
-    signature.splice(n, 3 - n);
+
+    if (candidate && arr[i] - arr[i - 1] < 0) {
+      result.pos.push(candidate.pos);
+      result.peaks.push(candidate.peaks);
+      candidate = null;
+    }
   }
-  return signature;
+
+  return result;
 }
-console.log(tribonacci([1, 1, 1], 1));
+
+console.log(pickPeaks([1, 2, 3, 6, 4, 1, 2, 3, 2, 1]));
