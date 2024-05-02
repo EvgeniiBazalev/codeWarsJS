4727ca0d6a762f73ad3a544d700daaa62295d0f6
EvgeniiBazalev <bazalevj@gmail.com>
2024-05-02 09:34:49 +0500

    3

--- 2024-04-25.js
+++ 2024-04-25.js
@@ -1,17 +1,11 @@
-function deleteNth(arr, n) {
-  let statistic = {};
-  let clearedArr = [];
-
-  arr.forEach((item) => {
-    if (statistic[item]) {
-      statistic[item]++;
-    } else {
-      statistic[item] = 1;
+function tribonacci(signature, n) {
+  if (n > 3) {
+    for (i = 0; i < n - 3; i++) {
+      signature.push(signature[i + 0] + signature[i + 1] + signature[i + 2]);
     }
-
-    if (statistic[item] <= n) clearedArr.push(item);
-  });
-
-  return clearedArr;
+  } else {
+    signature.splice(n, 3 - n);
+  }
+  return signature;
 }
-console.log(deleteNth([20, 37, 20, 21], 1));
+console.log(tribonacci([1, 1, 1], 1));
