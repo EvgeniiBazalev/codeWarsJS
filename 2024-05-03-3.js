8a43b6bda3f0d0ad030a295312746867923714ac
EvgeniiBazalev <bazalevj@gmail.com>
2024-05-15 17:40:42 +0500

    @

--- 2024-05-03-3.js
+++ 2024-05-03-3.js
@@ -1,8 +1,20 @@
 function getPINs(observed) {
+  let variables = {
+    1: [2, 4],
+    2: [1, 3, 5],
+    3: [2, 6],
+    4: [1, 5, 7],
+    5: [2, 4, 6, 8],
+    6: [3, 5, 9],
+    7: [4, 8],
+    8: [5, 7, 9, 0],
+    9: [6, 8],
+    0: [8],
+  };
   return observed;
 }
 
-console.log(getPINs(12));
+console.log(getPINs(5));
 /* 
 ┌───┬───┬───┐
 │ 1 │ 2 │ 3 │
