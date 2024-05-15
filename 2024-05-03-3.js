eefbad6fe1376d0c7b0b161c5eab3559ac6bf22a
EvgeniiBazalev <bazalevj@gmail.com>
2024-05-15 17:07:57 +0500

    2024.05.15

--- 2024-05-03-3.js
+++ 2024-05-03-3.js
@@ -1,47 +1,15 @@
-function sumIntervals(intervals) {
-  intervals.sort((a, b) => a[0] - b[0] || b[1] - a[1]);
-  intervals = intervals.filter((item) => item[0] != item[1]);
-  console.log(intervals);
-  for (i = 0; i < intervals.length; i++) {
-    if (
-      intervals[i + 1] &&
-      intervals[i + 1][0] >= intervals[i][0] &&
-      intervals[i + 1][1] <= intervals[i][1]
-    ) {
-      intervals.splice(i + 1, 1);
-      i--;
-      continue;
-    }
-
-    if (intervals[i + 1] && intervals[i][1] > intervals[i + 1][0]) {
-      intervals[i][1] = intervals[i + 1][0];
-    }
-    if (intervals[i + 1] && intervals[i][1] > intervals[i + 1][1]) {
-      intervals[i + 1][1] = intervals[i + 1][0];
-    }
-  }
-
-  console.log(intervals);
-
-  let allIntervals = intervals.reduce((accumulator, item) => {
-    return accumulator + item[1] - item[0];
-  }, 0);
-
-  return allIntervals;
+function getPINs(observed) {
+  return observed;
 }
-console.log(
-  sumIntervals([
-    [1, 20],
-    [2, 19],
-    [5, 15],
-    [8, 12],
-  ])
-);
-/* console.log(
-    sumIntervals([
-      [1, 20],
-      [2, 19],
-      [5, 15],
-      [8, 12],
-    ])
-  ); */
+
+console.log(getPINs(12));
+/* 
+┌───┬───┬───┐
+│ 1 │ 2 │ 3 │
+├───┼───┼───┤
+│ 4 │ 5 │ 6 │
+├───┼───┼───┤
+│ 7 │ 8 │ 9 │
+└───┼───┼───┘
+    │ 0 │
+    └───┘ */
