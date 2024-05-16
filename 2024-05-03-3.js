0688cd005428ee4a66f92b5b4b0dd94e1bb93265
EvgeniiBazalev <bazalevj@gmail.com>
2024-05-16 14:58:36 +0500

    next cata

--- 2024-05-03-3.js
+++ 2024-05-03-3.js
@@ -1,20 +1,40 @@
+let variables = {
+  1: [1, 2, 4],
+  2: [2, 1, 3, 5],
+  3: [3, 2, 6],
+  4: [4, 1, 5, 7],
+  5: [5, 2, 4, 6, 8],
+  6: [6, 3, 5, 9],
+  7: [7, 4, 8],
+  8: [8, 5, 7, 9, 0],
+  9: [9, 6, 8],
+  0: [0, 8],
+};
+
 function getPINs(observed) {
-  let variables = {
-    1: [2, 4],
-    2: [1, 3, 5],
-    3: [2, 6],
-    4: [1, 5, 7],
-    5: [2, 4, 6, 8],
-    6: [3, 5, 9],
-    7: [4, 8],
-    8: [5, 7, 9, 0],
-    9: [6, 8],
-    0: [8],
-  };
-  return observed;
+  observed = observed.toString();
+
+  let result = [];
+
+  function pushInResult(string, comboRes) {
+    for (let i = 0; i < string.length; i++) {
+      let combination = comboRes;
+      if (string.length > 1) {
+        pushInResult(string.slice(1), combination);
+      } else {
+        for (let j = 0; j < variables[observed[i]].length; j++) {
+          result.push(combination + variables[observed[i]][j]);
+        }
+      }
+    }
+  }
+
+  pushInResult(observed, "");
+
+  return result;
 }
 
-console.log(getPINs(5));
+console.log(getPINs(123));
 /* 
 ┌───┬───┬───┐
 │ 1 │ 2 │ 3 │
